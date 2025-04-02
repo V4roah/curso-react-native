e49f70528a2642bef2dacc0d14d9b5f6841f8d58
v4roah <dafvargasro@gmail.com>
2025-04-02 17:53:57 -0500

    [add] create project and file metacritic.js

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { getLatestGames } from "./lib/metacritic";
+
+// const icon = require("./assets/favicon.png");
 
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then((games) => setGames(games));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+
+      {games.map((game) => (
+        <View key={game.slug} style={styles.card}>
+          <Image
+            source={{ uri: game.image }}
+            style={{ width: 107, height: 147, borderRadius: 10 }}
+          />
+          <Text style={styles.title}>{game.title}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +32,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    backgroundColor: "#222",
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 5,
   },
 });
